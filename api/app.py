f338c5a3a0e3c07c24eee88cc0f9754dbdb015f3
juliaedwiges <74474146+juliaedwiges@users.noreply.github.com>
2025-06-02 10:45:53 -0300

    Update app.py

--- api/app.py
+++ api/app.py
@@ -5,36 +5,31 @@ import io
 import torch
 from torchvision import transforms, models
 import torch.nn as nn
-import uvicorn
 import os
 
 app = FastAPI()
 
-# Defina as classes conforme seu dataset
 classes = ['CNH_Aberta', 'CNH_Frente', 'CNH_Verso', 'CPF_Frente', 'CPF_Verso', 'RG_Aberto', 'RG_Frente', 'RG_Verso']
 
-# Transformações da imagem para o modelo
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225]),
 ])
 
-# Caminho para o modelo
-model_path = os.path.join('model', 'document_classifier.pth')
+device = torch.device("cpu")
 
-# Reconstrua a arquitetura do modelo
-def get_model():
+# Carrega modelo globalmente no startup
+@app.on_event("startup")
+def load_model():
+    global model
+    model_path = os.path.join('model', 'document_classifier.pth')
     model = models.resnet18(pretrained=False)
     num_ftrs = model.fc.in_features
-    model.fc = nn.Linear(num_ftrs, len(classes))  # 8 classes
-    return model
-
-# Inicialize e carregue os pesos
-device = torch.device("cpu")
-model = get_model()
-model.load_state_dict(torch.load(model_path, map_location=device))
-model.eval()
+    model.fc = nn.Linear(num_ftrs, len(classes))
+    model.load_state_dict(torch.load(model_path, map_location=device))
+    model.eval()
 
 @app.post("/predict/")
 async def predict(file: UploadFile = File(...)):
